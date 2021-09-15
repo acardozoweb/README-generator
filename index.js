12661524067fa3b11205dec4ad24f01b1b1d5ab7
anthonycc <anthonyccardozo@gmail.com>
2021-09-15 14:23:27 -0400

    finished markdown outline

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = () => {
@@ -57,7 +57,7 @@ const questions = () => {
 
 // TODO: Create a function to write README file
 const writeToFile = (fileName, data) => {
-    
+
 }
 
 // TODO: Create a function to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,16 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 const renderLicenseBadge = (license) => {
-  if (license) {
-    return
+  if (license === "none") {
+    return "";
   } else {
     return `![License](https://img.shields.io/badge/License-${license}-<blue>)`;
   }
 };
 
-// TODO: Create a function that returns the license link
+// function that returns the license link
 // If there is no license, return an empty string
-const renderLicenseLink = (license) => {
+const renderLicenseLink = license => {
   if (license === "none") {
     return "";
   } else {
@@ -18,7 +18,7 @@ const renderLicenseLink = (license) => {
   }
 };
 
-// TODO: Create a function that returns the license section of README
+// function that returns the license section of README
 // If there is no license, return an empty string
 const renderLicenseSection = (license) => {
   if (license === "none") {
@@ -30,39 +30,55 @@ const renderLicenseSection = (license) => {
   }
 }
 
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 const generateMarkdown = data => {
   return `# ${data.title}
 
   ${renderLicenseBadge(data.license)}
 
   ## Description
+
   ${data.description}
 
 
   ## Table of Contents
 
   * [Installation] (#installation)
+
+  * [Usage] (#usage)
+  ${renderLicenseLink(data.license)}
+
+  * Contributions
+
+  * [Testing] (#testing)
+
+  * [Questions] (#questions)
+
+  ## Installation
   To install program, run the following:
 
   \`\`\`
   ${data.installation}
   \`\`\`
 
-  * [Usage] (#usage)
-  ${renderLicenseLink(data.license)}
+  ## Usage
+
+  ${data.usage}
+  ${renderLicenseSection(data.license)}
 
   ## Contributions
+
   ${data.contribution}
 
   ## Testing
-  To test the program, run the following:
+  Run this command to test the app::
   
   \`\`\`
   ${data.test}
   \`\`\`
 
   ## Questions
+
   View more of my work at
   [${data.github}](https://github.com/${data.github}/)
 
